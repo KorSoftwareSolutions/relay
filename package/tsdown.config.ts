c9a958311d68980cc38ee861e4222cb1720a9515
ionkorol <ion@korol.dev>
2025-12-16 12:33:48 -0500

    update exports

--- package/tsdown.config.ts
+++ package/tsdown.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: ["./src/index.ts"],
   external: ["expo-clipboard", "expo-device", "expo-localization", "react-native"],
+  outDir: "dist",
+  dts: true,
+  exports: {
+    devExports: "dev-source",
+  },
   onSuccess() {
     console.info("🙏 Build succeeded!");
   },
